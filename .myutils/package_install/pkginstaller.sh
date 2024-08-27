f15cfaba10d022ab28535419149a880ea695e74a
Jacopo Giusti <pcb060@protonmail.com>
2024-08-28 00:53:19 +0200

    Fix bootstrap scripts

--- .myutils/package_install/pkginstaller.sh
+++ .myutils/package_install/pkginstaller.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
-echo "Executing pkginstaller.sh on folder $1"
+echo "Executing pkginstaller on folder '$1'"
 for file in "$1"/*; do
   case "$file" in
     *.apt)
-      echo "  Installing packages in "$file" using apt..."
+      echo "  Installing packages in '"$file"' using apt..."
       sudo apt install -y $(egrep --invert-match "^(#|$)" "$file")
       ;;
     *.snap)
-      echo "  Installing packages in "$file" using snap..."
+      echo "  Installing packages in '"$file"' using snap..."
       while IFS= read -r pkg; do sudo snap install "$pkg"; done < "$file" 
       ;;
     *.sh)
-      echo "  Installing package by executing shell script "$file"..."
+      echo "  Installing package by executing shell script '"$file"'..."
       "$file"
       ;;
     *)
-      echo "  Skipping unrecognized file type: $file"
+      echo "  Skipping unrecognized file type: '$file'"
       ;;
   esac
 done
