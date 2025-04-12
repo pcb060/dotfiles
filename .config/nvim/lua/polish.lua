6e5000c3a22cd7fd69bcc2decbb7cbcd89beea4a
Jacopo Giusti <pcb060@protonmail.com>
2025-04-12 13:44:37 +0200

    Enable polish file and set custom shell script detection

--- .config/nvim/lua/polish.lua
+++ .config/nvim/lua/polish.lua
@@ -1,18 +1,26 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+local M = {}
 
--- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  callback = function(args)
+    local buf = args.buf
+    local line = vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1] or ""
 
--- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+    local shebangs = {
+      "^#!%s*/bin/sh",
+      "^#!%s*/bin/bash",
+      "^#!%s*/usr/bin/env%s+sh",
+      "^#!%s*/usr/bin/env%s+bash",
+      "^#!%s*/bin/zsh",
+      "^#!%s*/usr/bin/env%s+zsh",
+    }
+
+    for _, pattern in ipairs(shebangs) do
+      if line:match(pattern) then
+        vim.bo[buf].filetype = "sh"
+        return
+      end
+    end
+  end,
+})
+
+return M
