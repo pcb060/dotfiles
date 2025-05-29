9b1a58d69733841dafc7b2175fc077c066b22755
Jacopo Giusti <pcb060@protonmail.com>
2025-05-29 16:51:18 +0200

    Update functions + add xc/xcf with xsel implementation

--- .myutils/functions.sh
+++ .myutils/functions.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
+cBLACK="$(tput setaf 0)"
+cRED="$(tput setaf 1)"
+cGREEN="$(tput setaf 2)"
+cYELLOW="$(tput setaf 3)"
+cBLUE="$(tput setaf 4)"
+cMAGENTA="$(tput setaf 5)"
+cCYAN="$(tput setaf 6)"
+cWHITE="$(tput setaf 7)"
+cRESET="$(tput sgr0)"
 
 # kill all jps services with matching name
 function killbyname() {
-  kill -9 `jps | grep "$1" | cut -d " " -f 1` 2> /dev/null
+  kill -9 "$(jps | grep "$1" | cut -d " " -f 1)" 2> /dev/null
   if [ "$?" -eq 0 ]; then
     echo "killbyname: Process \"$1\" killed succesfully"
   else
@@ -11,9 +20,27 @@ function killbyname() {
 }
 
 function hg() {
-  history | grep --color=always $1 | grep -v ' hg ' | cbat --style=grid,numbers,snip
+  history | grep --color=always "$1" | grep -v ' hg ' | cbat --style=grid,numbers,snip
 }
 
 function rbat() {
-	tac $1 | bat
+	tac "$1" | bat
+}
+
+# xsel implementation
+xc() {
+  local input
+  input=$(cat)
+  printf "%s" "$input" | xsel --clipboard --input
+  echo "📋 Copied to clipboard: ${input:0:60}$( [ ${#input} -gt 60 ] && echo "…")"
+}
+
+# xsel implementation
+xcf() {
+  if [[ -f "$1" ]]; then
+    xsel --clipboard --input < "$1"
+    echo "📋 Copied file to clipboard: $1"
+  else
+    echo "❌ File not found: $1"
+  fi
 }
