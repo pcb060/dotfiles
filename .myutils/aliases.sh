2205cc020f6dac795c81201c67bfba7172273d1a
Jacopo Giusti <pcb060@protonmail.com>
2026-02-25 16:42:25 +0100

    Add docker aliases + refactor

--- .myutils/aliases.sh
+++ .myutils/aliases.sh
@@ -1,42 +1,50 @@
 #!/usr/bin/env bash
 
 # GENERAL #
-  alias clc="clear"
-  alias l="ll"
-  alias la="ll -a"
-  alias lla="la"
-  alias lr="ll -R"
-  alias lt="ll -T"
-  alias lT="ll --total-size"
+alias clc="clear"
+alias l="ll"
+alias la="ll -a"
+alias lla="la"
+alias lr="ll -R"
+alias lt="ll -T"
+alias lT="ll --total-size"
 
 # UTILS #
-  alias mi="micro"
-  alias bat="batcat"
-    alias b="bat"
-    alias cbat="bat --paging=never"
-  alias r="ranger"
-  alias v="nvim"
-    alias n="v"
-    alias neovim="v"
-  alias kbn="killbyname"
-  alias ls="eza --git --icons=always --smart-group" # override
-  alias df="duf" # override
-  alias ps="procs" # override
-  alias ping="gping" #override
-  alias fd="fdfind"
-  alias lg="lazygit"
-  alias tm="tmux new-session -A -s main"
-  	alias tmrw="tmux rename-window"
-  alias tally="sort | uniq -c | sort"
-  alias gogh='bash -c "$(wget -qO- https://git.io/vQgMr)"'
-  alias dvenv="deactivate"
+alias mi="micro"
+alias b="bat"
+  alias cbat="bat --paging=never"
+alias r="ranger"
+alias v="nvim"
+  alias n="v"
+  alias neovim="v"
+alias kbn="killbyname"
+alias ls="eza --git --icons=always --smart-group" # override
+alias df="duf" # override
+alias ps="procs" # override
+alias ping="gping" #override
+alias fd="fdfind"
+alias lg="lazygit"
+alias tm="tmux new-session -A -s main"
+  alias tmrw="tmux rename-window"
+alias tally="sort | uniq -c | sort"
+alias gogh='bash -c "$(wget -qO- https://git.io/vQgMr)"'
+alias dvenv="deactivate"
   
 # NAVIGATION #
-  alias ..="cd .."
-  alias ...="cd ../.."
-  alias ....="cd ../../.."
-  alias cdsha="cd $SHARED"
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
 
 # META #
-  alias mys="echo -n 'Sourcing .zshrc... ' && source ~/.zshrc && echo 'OK!'"
+alias mys="echo -n 'Sourcing .zshrc... ' && source ~/.zshrc && echo 'OK!'"
+
+# DOCKER, PODMAN & DOCKER-COMPOSE #
+alias dcd="docker-compose down"
+  alias dcdo="docker-compose down --remove-orphans"
+alias dcu="docker-compose up"
+  alias dcud="docker-compose up -d"
+
+# K8S #
+alias k3stop="sudo systemctl stop k3s"
+alias k3start="sudo systemctl start k3s"
 
