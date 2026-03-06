3b913baf81894e19ba8fdd27b8695cd2bef52161
Jacopo Giusti <pcb060@protonmail.com>
2026-03-06 22:41:18 +0100

    Add 'jac' scripts

--- .myutils/functions.sh
+++ .myutils/functions.sh
@@ -96,3 +96,170 @@ venv() {
   { source "$venv_dir/bin/activate" && pGreen "Activated virtual environment in '$venv_dir'." ; } \
     || pRed "Failed to activate virtual environment."
 }
+
+# Print size of file in human-readable format
+hrs() {
+  usage() {
+    echo "Usage: hrs [options] <file>"
+    echo "Options:"
+    echo "  -h, --help  Display this help message"
+    exit 1
+  }
+
+  if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    usage
+  fi
+
+  stat --format=%s $1 | numfmt --to=si --suffix=B
+}
+
+# Download template gitignore file for specified language
+ignore() {
+  usage() {
+    echo "Usage: ignore [-i] language"
+    exit 1
+  }
+
+  if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+    usage
+  fi
+
+  repo_url="https://github.com/github/gitignore"
+  branch="main"
+  raw_url="$repo_url/raw/$branch"
+
+  check_cert_param=""
+  lang=""
+  if [ "$#" -eq 2 ]; then
+    if [ "$1" == "-i" ]; then
+      check_cert_param="--no-check-certificate"
+      lang="$2"
+    else
+      echo "Invalid option: $1" 
+      usage
+    fi
+  else
+    lang="$1"
+  fi
+
+  lang=${lang^}
+
+  wget "$check_cert_param" -O ".gitignore" "$raw_url/$lang.gitignore" \
+    || echo "Download of template .gitignore file for language $lang failed"
+}
+
+# Execute command on all directories at current position
+fad() {
+  local script_path="$(readlink -f "${BASH_SOURCE[0]}")"
+  local script_dir="$(dirname "$script_path")"
+  source "$script_dir/../utils/colors"
+  
+  local comm; comm="$@"
+  
+  for dir in */; do
+    [ -d "$dir" ] || continue
+    printf "%bExecuting %b\"%s\"%b in directory %b%s%b\n" "$cYELLOW" "$cCYAN" "$comm" "$cYELLOW" "$cBLUE" "${dir//\//}" "$cRESET"
+    (cd "$dir" && "$@" && echo)
+  done
+}
+
+# Create template script file fitting bash best practices
+script() {
+  usage() {
+    echo "Usage: script [-x] <filename>"
+    echo "Options:"
+    echo "  -x  Make file executable"
+    exit 1
+  }
+
+  # Check if the user provided a filename and optional -x flag
+  if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+      usage
+  fi
+
+  # Initialize variables
+  make_executable=false
+  filename=""
+
+  # Handle arguments
+  if [ "$#" -eq 2 ]; then
+      if [ "$1" == "-x" ]; then
+          make_executable=true
+          filename="$2"
+      else
+          echo "Invalid option: $1"
+          usage
+      fi
+  else
+      filename="$1"
+  fi
+
+  # Create the bash script with shebang and set options
+  echo '#!/usr/bin/env bash' > "$filename"
+  echo 'set -euo pipefail' >> "$filename"
+
+  # Make the script executable if -x flag was provided
+  if [ "$make_executable" = true ]; then
+      chmod +x "$filename"
+      echo "Bash script '$filename' created and made executable."
+  else
+      echo "Bash script '$filename' created."
+  fi
+}
+
+# A workaround for a bug where yadm won't add files
+# already in another git repo (probably a git issue).
+#
+# https://github.com/TheLocehiliosan/yadm/issues/361
+#
+# Run this from the base dir with the conflicting .git/ in it.
+# Example:
+#   ~/.oh-my-zsh $ myscript yadm-force-add custom/plugins/install.sh
+yadm-force-add() {
+
+  # Must be in the root git dir.
+  if [[ ! -d ".git" ]]; then
+      echo
+      echo "Run from the conflicted .git repo base dir."
+      echo "(I'm looking for a .git/ dir.)"
+      echo
+      exit 1
+  fi
+
+  tmpdir=/tmp/tmp.git
+
+  if [[ -d $tmpdir ]]; then
+      echo
+      echo "Yo the temp dir already exists. Did I crash?"
+      echo "Please reconcile: $tmpdir"
+      echo
+      exit 1
+  fi
+
+  # Git is just files.
+  # So just move the .git dir away for a second.
+  mv .git $tmpdir
+
+  # yadm will respect the other repo's .gitignore file
+  # so be sure to move that too.
+  tmpignore=/tmp/tmp.gitignore
+
+  if [[ -e ".gitignore" ]]; then
+      mv .gitignore $tmpignore
+  fi
+
+  # Should work now.
+  yadm add "$1"
+
+  # Put the .git dir back.
+  mv $tmpdir .git
+
+  # Put the .gitignore back (if it existed).
+  if [[ -e $tmpignore ]]; then
+      mv $tmpignore .gitignore
+  fi
+
+  # Show the yadm status, hopefully with a successfully added file.
+  yadm status
+}
+
